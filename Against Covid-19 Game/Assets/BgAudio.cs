ff61d5ec56272c19112505acd6b58f756c14aeaf
NapTech Games <naptechgames@gmail.com>
2020-05-10 13:02:37 +0600

    Icon Set

---
+++ Against Covid-19 Game/Assets/BgAudio.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgAudio : MonoBehaviour
+{
+
+    //Play Global
+    private static BgAudio instance = null;
+    public static BgAudio Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+     void Update()
+    {
+        // Make sure user is on Android platform
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                // Quit the application
+                Application.Quit();
+            }
+        }
+    }
+
+}
